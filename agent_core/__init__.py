3e172a4117cd51807d3467973d950bc02bd317b2
蒋德旺 <jiangdewang@migu.chinamobile.com>
2026-05-13 11:53:41 +0800

    chore: create agent_core package skeleton

---
+++ agent_core/__init__.py
@@ -0,0 +1,3 @@
+"""agent-core: general-purpose Python agent framework."""
+
+__version__ = "0.1.0"
---
+++ tests/test_package_imports.py
@@ -0,0 +1,6 @@
+def test_package_imports():
+    import agent_core  # noqa: F401
+    import agent_core.core  # noqa: F401
+    import agent_core.providers  # noqa: F401
+    import agent_core.tools  # noqa: F401
+    import agent_core.session  # noqa: F401
